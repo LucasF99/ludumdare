977e3164afc3644fcc06ad27677456f1ae46313a
Daniel Zimmer <daniel_zimmer@outlook.com>
2018-04-22 02:42:28 -0300

    Agora o pirilampo salta
    O nome dele é a partir de agora definitivamente Pirilampo

--- main.lua
+++ main.lua
@@ -4,7 +4,8 @@
 ------Variables------
 ---system---
 local fullScreenState = false
-time = 0
+local _, _, flags = love.window.getMode()
+WIDTH, HEIGHT = love.window.getDesktopDimensions(flags.display)
 
 ----keys----
 local keys = {
@@ -13,34 +14,37 @@ local keys = {
 ----game----
 local bgColor = {0.2, 0.6, 0.8}
 
+
 ---------------------
 
 function love.load()
-    local _, _, flags = love.window.getMode()
-    WIDTH, HEIGHT = love.window.getDesktopDimensions(flags.display)
-    
-    
+    ------Initializations------
+    ---player---
     player = require "player"
-    
-    love.graphics.setDefaultFilter( "nearest" )
+    love.graphics.setDefaultFilter("nearest")
     for i = 1, 17, 1 do
       player.setSprite(love.graphics.newImage("res/player/player_".. i .. ".png"), i)
     end
+    ------------
+    
     
     ------Setups------
     love.graphics.setBackgroundColor(bgColor)
     love.window.setMode(WIDTH, HEIGHT, {resizable=true, vsync=false, borderless=true, fullscreen = fullScreenState})
+    love.mouse.setVisible(false)
 end
   
 function love.update(dt)
-   player.uFrame(dt)
-   player.move(dt)
+    player.update(dt)
+    
 end
   
 function love.draw()
-    love.graphics.draw(player.getFrame(), player.getPx(), player.getPy(), 0, (WIDTH/1920)*5.5, (WIDTH/1920)*5.5)
+    player.draw()
     
-    love.graphics.print("", 100, 100)
+    love.graphics.setColor(0.4, 0.4, 0.4)
+    love.graphics.rectangle("fill", 0 , (HEIGHT/1080)*900, WIDTH, (HEIGHT/1080)*(HEIGHT - 900))
+    
 end
 
 function love.keypressed(key)
@@ -49,8 +53,8 @@ function love.keypressed(key)
       love.event.quit()
     end
     
-end
-
-function rgb(r, g, b)
-    return r/255, g/255, b/255
+    if key == player.getJumpKey() then
+      player.jumped()
+    end
+    
 end
--- player.lua
+++ player.lua
@@ -1,5 +1,18 @@
 local player = {}
-local data = {sprite = {}, hp = 10, time = 17, px = 500, py = 800, velocity = (WIDTH/1920)*800,  keys = {right = "right", left = "left"}, isMoving = false}
+local data = {
+  sprite = {},
+  hp = 10,
+  px = 500, py = 812,
+  size = (WIDTH/1920)*5.5,
+  velocity = (WIDTH/1920)*800,  
+  keys = {right = "right", left = "left", jump = "space"},
+  time = 17,
+  moving = false,
+  jumping = false,
+  jumpInitSpeed = 25,
+  jumpSpeed = 30,
+  jumpDecay = 1
+}
 
 function player.setSprite(a, num)
     data.sprite[num] = a
@@ -31,16 +44,30 @@ function player.getHp()
     return data.hp
 end
 function player.setVelocity(a)
-    data.velocity = a
+    data.velocity = (WIDTH/1920)*a
 end
-function player.getVelocity(a)
+function player.getVelocity()
     return data.velocity
+end
+function player.getJumpKey()
+    return data.keys.jump
+end
+function player.setSize(a)
+    data.size = a*(WIDTH/1920)
+end
+
+function player.getSize()
+    return data.size
+end
+function player.jumped()
+    jumping = true
+    data.jumpSpeed = data.jumpInitSpeed
 end
 --
 function player.getFrame()
-    if data.isMoving == "right" then
+    if data.moving == "right" then
       return player.getSprite(math.floor(data.time)%((#data.sprite-1)/2) + 1)
-    elseif data.isMoving == "left" then
+    elseif data.moving == "left" then
       return player.getSprite(math.floor(data.time)%((#data.sprite-1)/2) + 1 + (#data.sprite-1)/2)
     else
       return player.getSprite(17)
@@ -52,16 +79,39 @@ end
 
 --
 function player.move(dt)
-    if love.keyboard.isDown(data.keys.right) and not love.keyboard.isDown(data.keys.left) and data.px < WIDTH - (data.sprite[17]:getWidth())*5 then
+    if love.keyboard.isDown(data.keys.right) and not love.keyboard.isDown(data.keys.left) and data.px < WIDTH - (data.sprite[17]:getWidth())*player.getSize() then
       data.px = data.px + data.velocity*dt
-      data.isMoving = "right"
+      data.moving = "right"
     elseif love.keyboard.isDown(data.keys.left) and not love.keyboard.isDown(data.keys.right)  and data.px > 0  then
       data.px = data.px - data.velocity*dt
-      data.isMoving = "left"
+      data.moving = "left"
     else
-      data.isMoving = false
+      data.moving = false
+    end
+    
+    if jumping == true then
+      
+      if data.jumpSpeed > -data.jumpInitSpeed then
+        data.py = data.py - data.jumpSpeed*70*dt
+        data.jumpSpeed = data.jumpSpeed - data.jumpDecay*95*dt
+      else
+        data.jumpSpeed = 0
+        data.py = 812
+        jumping = false
+      end
     end
     
 end
 
+function player.update(dt)
+    player.uFrame(dt)
+    player.move(dt)
+    
+end
+
+function player.draw()
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(player.getFrame(), player.getPx(), player.getPy(), 0, player.getSize(), player.getSize())
+end
+--
 return player
