e7d9fd40ce5f83260bcb7db7476a65232bf3d606
LucasF99 <lucasf0620@hotmail.com>
2018-04-22 15:42:06 -0300

    predios

---
+++ buildings.lua
@@ -0,0 +1,61 @@
+local buildings = {}
+local data = {
+  towers = {},
+  images = {},
+  twMult = (WIDTH/1920)*4.5,
+  fhMult = (WIDTH/1920)*4.5,
+  tw = (WIDTH/1920)*4.5*32,-- largura de cada torre
+  fh = (WIDTH/1920)*4.5*32,-- altura de cada andar
+  keys = {build = "down"}
+}
+
+function buildings.initTowers()
+    for i = 1, math.ceil(WIDTH/data.tw), 1 do
+      data.towers[i] = {}
+    end
+end
+
+function buildings.build(px, py)
+    index = math.ceil(px/data.tw)
+    table.insert(data.towers[index], 1)
+end
+
+function buildings.addImage(img)
+    table.insert(data.images, img)
+end
+
+function buildings.getFloorImage(towerIndex, floorIndex)
+    return data.images[data.towers[towerIndex][floorIndex]]
+end
+
+function buildings.getFh()
+    return data.fh
+end
+
+function buildings.getTw()
+    return data.tw
+end
+
+function buildings.getFhMult()
+    return data.fhMult
+end
+
+function buildings.getTwMult()
+    return data.thMult
+end
+
+function buildings.getBuildKey()
+    return data.keys.build
+end
+
+function buildings.draw()
+    for i = 1, #data.towers, 1 do
+      for j = 1, #data.towers[i], 1 do
+        love.graphics.setColor(1,1,1)
+        love.graphics.draw(buildings.getFloorImage(i, j), (i-1)*buildings.getTw(),
+          HEIGHT-((j-1)*buildings.getFh()), 0, buildings.getTwMult(), buildings.getFhMult())
+      end
+    end
+end
+
+return buildings
--- main.lua
+++ main.lua
@@ -21,13 +21,23 @@ function love.load()
     ------Initializations------
     ---player---
     player = require "player"
+    
     love.graphics.setDefaultFilter("nearest")
     for i = 1, 17, 1 do
       player.setSprite(love.graphics.newImage("res/player/player_".. i .. ".png"), i)
     end
+    
     player.setAudio(1 , love.audio.newSource("res/audio/step.mp3", "static")) 
     player.setAudio(2 , love.audio.newSource("res/audio/jump.mp3", "static"))
     player.setPy(900 - player.getSprite(17):getHeight()*player.getSize())
+    ------------
+    
+    ---buildings---
+    buildings = require "buildings"
+    
+    buildings.initTowers()
+    
+    buildings.addImage(love.graphics.newImage("res/buildings/residential_1.png"))
     ------------
     
     
@@ -48,6 +58,7 @@ function love.draw()
     love.graphics.setColor(0.4, 0.4, 0.4)
     love.graphics.rectangle("fill", 0 , (HEIGHT/1080)*900, WIDTH, (HEIGHT/1080)*(HEIGHT - 900))
     
+    buildings.draw()
 end
 
 function love.keypressed(key)
@@ -58,6 +69,10 @@ function love.keypressed(key)
     
     if key == player.getJumpKey() then
       player.jumped()
+    end
+    
+    if key == buildings.getBuildKey() then
+      buildings.build(player.getPx(), player.getPy())
     end
     
 end
