f0b7f96a1e746811b835209e0c5d3b23d72ec4ad
Daniel Zimmer <daniel_zimmer@outlook.com>
2018-04-21 21:54:08 -0300

    main que funfa

---
+++ main.lua
@@ -0,0 +1,14 @@
+------Ludum Dare------
+----Bra1 e Zimmer----
+
+function love.load()
+end
+  
+function love.update(dt)
+   --Essa callback e a love.draw() rodam intermitentemente e dt é inversamente proporcional a velocidade do pc
+end
+  
+function love.draw()
+    
+end
+
