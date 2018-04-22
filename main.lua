4e1019834728d5492a9c69078f0bb4e74a28470d
Daniel Zimmer <daniel_zimmer@outlook.com>
2018-04-21 22:29:37 -0300

    Melhor

--- main.lua
+++ main.lua
@@ -1,14 +1,45 @@
 ------Ludum Dare------
 ----Bra1 e Zimmer----
 
+------Variables------
+---system---
+local fullScreenState = false
+
+----keys----
+local keys = {
+    quit = "escape"
+}
+----game----
+local bgColor = {0.8, 0.4, 0.8}
+
+---------------------
+
 function love.load()
+    local _, _, flags = love.window.getMode()
+    WIDTH, HEIGHT = love.window.getDesktopDimensions(flags.display)
+    
+    
+    ------Setups------
+    love.graphics.setBackgroundColor(bgColor)
+    love.window.setMode(WIDTH, HEIGHT, {resizable=true, vsync=false, borderless=true, fullscreen = fullScreenState})
 end
   
 function love.update(dt)
-   --Essa callback e a love.draw() rodam intermitentemente e dt é inversamente proporcional a velocidade do pc
+   
 end
   
 function love.draw()
     
 end
 
+function love.keypressed(key)
+   --quits game if key.quit is pressed
+    if key == keys.quit then
+      love.event.quit()
+    end
+    
+end
+
+function rgb(r, g, b)
+    return r/255, g/255, b/255
+end
